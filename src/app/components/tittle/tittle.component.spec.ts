6a80a46dddc6ecfaf5b7ba0b7e21bb01768df874
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-13 01:46:46 +0300

    addnew all components and files

--- src/app/components/tittle/tittle.component.spec.ts
+++ src/app/components/tittle/tittle.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TitleComponent } from './title.component';
+import { TittleComponent } from './tittle.component';
 
-describe('TitleComponent', () => {
-  let component: TitleComponent;
-  let fixture: ComponentFixture<TitleComponent>;
+describe('TittleComponent', () => {
+  let component: TittleComponent;
+  let fixture: ComponentFixture<TittleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TitleComponent ]
+      declarations: [ TittleComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TitleComponent);
+    fixture = TestBed.createComponent(TittleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
