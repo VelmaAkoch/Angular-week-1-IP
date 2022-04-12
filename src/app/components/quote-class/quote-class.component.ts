343c5921986272a1994a116c019a980253bd6d2a
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-12 22:25:26 +0300

    add all files

---
+++ src/app/components/quote-class/quote-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-class',
+  templateUrl: './quote-class.component.html',
+  styleUrls: ['./quote-class.component.css']
+})
+export class QuoteClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
