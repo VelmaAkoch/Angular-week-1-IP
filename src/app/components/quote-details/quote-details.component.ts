ecb40fd177075092f7135481d7d35493bbbdf0fd
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-13 15:25:54 +0300

    add all changes

--- src/app/components/quote-details/quote-details.component.ts
+++ src/app/components/quote-details/quote-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+// import { EventEmitter } from 'stream';
+import { Quote } from "../quote";
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quora!: Quote;
+  @Output() hasRead = new EventEmitter<boolean>();
+  deleteQuote(read:boolean) {
+    this.hasRead.emit(read);
+  }
+  upvote() {
+    this.quora.likes+=1;
+  }
+  downvote() {
+    this.quora.dislikes+=1;
+  }
 
   constructor() { }
 
--- src/app/components/quotes-form/quotes-form.component.ts
+++ src/app/components/quotes-form/quotes-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Quote } from '../quotes';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes-form',
