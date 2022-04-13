9390d44118dd82b155606b8195924d04df91c1ec
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-13 03:21:03 +0300

    add all files in class quote-form

--- src/app/components/quote.ts
+++ src/app/components/quote.ts
@@ -1,7 +1,19 @@
-export class Quote {
+import { Quote } from "@angular/compiler";
+import { constructor } from "module";
+import { title } from "process";
+
+export class ng g c Quote {
     showDetails: boolean;
     showInfo:any
     constructor(public id:number, public name:string, public title:string, public quote:string, public author:string, public datePosted: Date, public likes:number, public dislikes:number) {
         this.showDetails=false;
     }
 }
+function public(public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) {
+    throw new Error("Function not implemented.");
+}
+
+function id(public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, id: any, number: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, name: void, string: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, title: string, string: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, quote: any, string: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, author: any, string: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, datePosted: any, Date: DateConstructor, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, likes: any, number: any, public: (public: any, id: any, number: any, public: any, name: void, string: any, public: any, title: any, string: any, public: any, quote: any, string: any, public: any, author: any, string: any, public: any, datePosted: any, Date: DateConstructor, public: any, likes: any, number: any, public: any, dislikes: any, number: any) => void, dislikes: any, number: any) {
+    throw new Error("Function not implemented.");
+}
+
